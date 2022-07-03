749ec097f44c841cd1ab00eef8ababaf735a6af1
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-07-03 14:15:16 -0300

    Criando a página Home e refatorando o Header

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -5,10 +5,30 @@ export const Container = styled.div`
 
 `;
 
+export const InputSearchContainer = styled.div`
+  width: 100%;
+
+  input {
+    width: 100%;
+    background: #fff;
+    border: none;
+    border-radius: 25px;
+    height: 50px;
+    //box-shadow: 0px 4px 18px rgba(0,0,0,0.5);
+    outline: 0;
+    padding: 0 16px;
+
+    &::placeholder {
+      color: #BCBCBC;
+    }
+  }
+`;
+
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin-top: 32px;
 
   strong {
     color: #222222;
