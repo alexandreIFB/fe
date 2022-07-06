4e7d136cebff9021535a059cf2f4f0c022433af9
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-07-06 16:34:23 -0300

    pegando os dados e lidando com o submit do form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,4 +1,5 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 import FormGroup from '../FormGroup';
 import { Form } from './styles';
 import { Input } from '../Input';
@@ -6,22 +7,53 @@ import { Select } from '../Select';
 import { Button } from '../Button';
 
 function ContactForm({ buttonLabel }) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [category, setCategory] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    console.log({
+      name,
+      email,
+      phone,
+      category,
+    });
+  }
+
   return (
-    <Form>
+    <Form onSubmit={(event) => handleSubmit(event)}>
       <FormGroup>
-        <Input placeholder="Nome" />
-      </FormGroup>
-      <FormGroup error="O formato do e-mail é invalido">
-        <Input placeholder="E-mail" error />
-      </FormGroup>
-      <FormGroup>
-        <Input placeholder="Telefone" />
+        <Input
+          placeholder="Nome"
+          value={name}
+          onChange={(event) => setName(event.target.value)}
+        />
       </FormGroup>
       <FormGroup>
-        <Select>
-          <option value="1">Instagram</option>
-          <option value="1">Instagram</option>
-          <option value="1">Instagram</option>
+        <Input
+          placeholder="E-mail"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Input
+          placeholder="Telefone"
+          value={phone}
+          onChange={(event) => setPhone(event.target.value)}
+        />
+      </FormGroup>
+      <FormGroup>
+        <Select
+          value={category}
+          onChange={(event) => setCategory(event.target.value)}
+        >
+          <option value="">Categoria</option>
+          <option value="INSTAGRAM">Instagram</option>
+          <option value="1">Discord</option>
         </Select>
       </FormGroup>
       <Button type="submit">{buttonLabel}</Button>
