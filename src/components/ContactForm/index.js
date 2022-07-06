f37697b932c56d22fe5f18ea00835574d84c194e
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-07-06 18:28:19 -0300

    Validando input name

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import FormGroup from '../FormGroup';
+
 import { Form } from './styles';
 import { Input } from '../Input';
 import { Select } from '../Select';
@@ -11,6 +12,36 @@ function ContactForm({ buttonLabel }) {
   const [email, setEmail] = useState('');
   const [phone, setPhone] = useState('');
   const [category, setCategory] = useState('');
+  const [erros, setErros] = useState([]);
+
+  function handleNameChange(event) {
+    setName(event.target.value);
+
+    if (!event.target.value) {
+      setErros((prevState) => [
+        ...prevState,
+        { field: 'name', message: 'Nome é obrigatorio' },
+      ]);
+    } else {
+      setErros((prevState) => prevState.filter(
+        (error) => error.field !== 'name',
+      ));
+    }
+  }
+
+  console.log(erros);
+
+  function handleEmailChange(event) {
+    return setEmail(event.target.value);
+  }
+
+  function handlePhoneChange(event) {
+    return setPhone(event.target.value);
+  }
+
+  function handleCategoryChange(event) {
+    return setCategory(event.target.value);
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -25,31 +56,34 @@ function ContactForm({ buttonLabel }) {
 
   return (
     <Form onSubmit={(event) => handleSubmit(event)}>
-      <FormGroup>
+      <FormGroup
+        error={erros.find((error) => error.field === 'name')?.message}
+      >
         <Input
           placeholder="Nome"
           value={name}
-          onChange={(event) => setName(event.target.value)}
+          onChange={handleNameChange}
+          error={erros.some((error) => error.field === 'name')}
         />
       </FormGroup>
       <FormGroup>
         <Input
           placeholder="E-mail"
           value={email}
-          onChange={(event) => setEmail(event.target.value)}
+          onChange={handleEmailChange}
         />
       </FormGroup>
       <FormGroup>
         <Input
           placeholder="Telefone"
           value={phone}
-          onChange={(event) => setPhone(event.target.value)}
+          onChange={handlePhoneChange}
         />
       </FormGroup>
       <FormGroup>
         <Select
           value={category}
-          onChange={(event) => setCategory(event.target.value)}
+          onChange={handleCategoryChange}
         >
           <option value="">Categoria</option>
           <option value="INSTAGRAM">Instagram</option>
