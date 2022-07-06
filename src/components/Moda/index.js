59f9182126c67aa7b3b7874ec473a08697e94d31
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-07-05 21:16:51 -0300

    entendendo e trabalhando com React Portals

--- src/components/Moda/index.js
+++ src/components/Moda/index.js
@@ -1,21 +1,25 @@
 import PropTypes from 'prop-types';
+import ReactDOM from 'react-dom';
 import { Button } from '../Button';
 import { Container, Footer, Overlay } from './styles';
 
 function Modal({ title, description, danger }) {
   return (
-    <Overlay>
-      <Container danger={danger}>
-        <h1>{title}</h1>
-        <p>{description}</p>
-        <Footer>
-          <button type="button" className="button-cancel">Cancelar</button>
-          <Button type="button" danger={danger}>
-            Deletar
-          </Button>
-        </Footer>
-      </Container>
-    </Overlay>
+    ReactDOM.createPortal(
+      <Overlay>
+        <Container danger={danger}>
+          <h1>{title}</h1>
+          <p>{description}</p>
+          <Footer>
+            <button type="button" className="button-cancel">Cancelar</button>
+            <Button type="button" danger={danger}>
+              Deletar
+            </Button>
+          </Footer>
+        </Container>
+      </Overlay>,
+      document.getElementById('modals-root'),
+    )
   );
 }
 
