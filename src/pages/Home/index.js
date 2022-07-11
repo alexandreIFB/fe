b3f7f7061a5d600669993316c80630a3c86e40cf
Alexandre Abreu <abreuxandi2010@gmail.com>
2022-07-11 18:50:51 -0300

    trabalhando com async/await dentro do useEffect

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -22,20 +22,27 @@ export default function Home() {
   ), [contacts, searchTerm]);
 
   useEffect(() => {
-    setIsLoading(true);
+    async function loadContacts() {
+      try {
+        setIsLoading(true);
 
-    fetch(`http://localhost:3001/contactss?orderBy=${orderBy}`)
-      .then(async (response) => {
+        const response = await fetch(
+          `http://localhost:3001/contacts?orderBy=${orderBy}`,
+        );
+
         await delay(500);
+
         const json = await response.json();
+
         setContacts(json);
-      })
-      .catch((error) => {
-        console.log('Erro', error);
-      })
-      .finally(() => {
+      } catch (error) {
+        console.log('error', error);
+      } finally {
         setIsLoading(false);
-      });
+      }
+    }
+
+    loadContacts();
   }, [orderBy]);
 
   function handleToggleOrderyBy() {
